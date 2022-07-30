a8d454cd36a179d257050369441a9b6bec4d995c
timatet <timofei.teterin@inbox.ru>
2022-07-30 20:22:13 +0300

    Small Fix

--- back java springboot/src/main/java/com/linkshortservice/linkshortservice/UrlController.java
+++ back java springboot/src/main/java/com/linkshortservice/linkshortservice/UrlController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
@@ -34,6 +35,7 @@ public class UrlController {
     }
 
     @PostMapping
+    @RequestMapping(value="/", method=RequestMethod.POST)
     public String generateShortUrl(@RequestBody String url) {
         return urlService.generateShortUrl(url);
     }
